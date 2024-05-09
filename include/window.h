b4a66fa5ac7557c98ae0cd0be125817797de32b7
clemO <clement.oberhauser@gmail.com>
2024-05-09 12:06:08 +0200

    Ajout Height et Width dans les attributs de la classe

--- include/window.h
+++ include/window.h
@@ -8,13 +8,17 @@ public:
     Window();
     ~Window();
 
-    int init(const char* title, int width, int height);
+    int init(const char* title);
     void quit();
     SDL_Renderer* getRenderer() const; // Ajout de getRenderer
 
+    int getWinHeight();
+    int getWinWidth();
 private:
     SDL_Window* fenetre;
     SDL_Renderer* renderer; // Ajout d'un pointeur vers le renderer
+    int windowHeight;
+    int windowWidth;
 };
 
 #endif // WINDOW_H
--- src/window.cpp
+++ src/window.cpp
@@ -1,19 +1,19 @@
 #include "../include/window.h"
 
-Window::Window() : fenetre(nullptr), renderer(nullptr) {
+Window::Window() : fenetre(nullptr), renderer(nullptr), windowHeight(480), windowWidth(640) {
 }
 
 Window::~Window() {
     quit();
 }
 
-int Window::init(const char* title, int width, int height) {
+int Window::init(const char* title) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_Log("Erreur lors de l'initialisation de SDL : %s", SDL_GetError());
         return -1;
     }
 
-    fenetre = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+    fenetre = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
     if (!fenetre) {
         SDL_Log("Erreur lors de la création de la fenêtre : %s", SDL_GetError());
         SDL_Quit();
@@ -44,4 +44,12 @@ void Window::quit() {
 
 SDL_Renderer* Window::getRenderer() const {
     return renderer;
+}
+
+int Window::getWinWidth(){
+    return windowWidth;
+}
+
+int Window::getWinHeight(){
+    return windowHeight;
 }
